468a8c9bd7c4f8e260846e0b580350a2ff83033c
acmerfight <acmerfight@gmail.com>
2015-05-24 16:49:51 +0800

    add basic example

---
+++ basic.erl
@@ -0,0 +1,12 @@
+-module(basic).
+-export([loop/0]).
+
+loop() ->
+    receive
+        {rectangle, Width, Ht} ->
+            io:format("Area of rectangle is ~p~n",[Width * Ht]),
+            loop();
+        {square, Side} ->
+            io:format("Area of square is ~p~n", [Side * Side]),
+            loop()
+   end.
