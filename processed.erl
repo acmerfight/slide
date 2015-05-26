87ff403dd84fc3e541f2ad02d3f0325008970d7e
acmerfight <acmerfight@gmail.com>
2015-05-26 23:42:44 +0800

    add process code

---
+++ processed.erl
@@ -0,0 +1,29 @@
+-module(processes).
+
+-export([max/1]).
+
+%% processes:max(300000)
+
+%% 创建N个进程然后销毁它们
+%% 看看需要花费多长时间
+
+max(N) ->
+    Max = erlang:system_info(process_limit),
+    io:format("Maximum allowed processes:~p~n",[Max]),
+    statistics(runtime),
+    statistics(wall_clock),
+    L = for(1, N, fun() -> spawn(fun() -> wait() end) end),
+    {_, Time1} = statistics(runtime),
+    {_, Time2} = statistics(wall_clock),
+    lists:foreach(fun(Pid) -> Pid ! die end, L),
+    U1 = Time1 * 1000 / N,
+    U2 = Time2 * 1000 / N,
+    io:format("Process spawn time=~p (~p) microseconds~n",
+               [U1, U2]).
+wait() ->
+    receive
+        die -> void
+    end.
+
+for(N, N, F) -> [F()];
+for(I, N, F) -> [F()|for(I+1, N, F)].
