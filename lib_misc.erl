d4b424bd9b3ca98ca2df4c8741e39db6253644f5
acmerfight <acmerfight@gmail.com>
2015-06-03 23:55:29 +0800

    add erlang example

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,6 +1,6 @@
 -module(lib_misc).
 
--export([on_exit/1, start/0]).
+-export([on_exit/2, start/0, log_error/2, keep_alive/2]).
 
 
 log_error(Pid, Why) ->
@@ -13,11 +13,15 @@ start() ->
     end.
 
 
-on_exit(Pid) ->
+on_exit(Pid, F) ->
     spawn(fun() ->
             Ref = monitor(process, Pid),
             receive
                 {'DOWN', Ref, process, Pid, Why} ->
-                    log_error(Pid, Why)
+                    F(Pid, Why)
             end
         end).
+
+keep_alive(Name, Fun) ->
+    register(Name, Pid = spawn(Fun)),
+    on_exit(Pid, fun(_Why) -> keep_alive(Name, Fun) end).
