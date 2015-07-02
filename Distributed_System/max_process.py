dbe59add3c0cd5d8fc77082acc57741b4b7cc39d
acmerfight <acmerfight@gmail.com>
2015-07-02 15:21:55 +0800

    make beeter dir

--- Distributed_System/max_process.py
+++ Distributed_System/max_process.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 
+import ipdb
 import time
 from multiprocessing import Pool
 
 
 def f(x):
     while 1:
-        a = 2 ** 10000000
+        time.sleep(100000)
 
 if __name__ == '__main__':
     p = Pool(200)
-    print(p.map(f, [1, 2, 3]))
+    print(p.map(f, [1, 2, 3] * 100000))
